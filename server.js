71ecbe2f1d4741380f1bc4cc0466e93881e19b28
seog-jun <shong69@myseneca.ca>
2022-02-04 17:06:02 -0500

    WIP endpoints/routes- lyrics query by ID, about, music, 404

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const musicData = require("./musicData.js");
 
 const HTTP_PORT = process.env.port || 8080;
 const onHTTPStart = () => {
@@ -14,8 +15,45 @@ app.get("/", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  //   res.send("hello home");
   res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.send("hello about");
+});
+
+app.get("/lyrics", (req, res) => {
+  musicData
+    .getAlbums()
+    .then((data) => {
+      res.json(data); // =res.send(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send("ERROR!");
+    });
+});
+
+app.get("/lyrics/:id", (req, res) => {
+  musicData
+    .getAlbums()
+    .then((data) => {
+      //   res.send(req.params.id]); // res.params.id is coming from end point that I write after /lyrics/123
+      // resolved promise Data[id from request params].field
+      res.send(data[req.params.id - 1].lyrics);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send("ERROR!");
+    });
+});
+
+app.get("/music", (req, res) => {
+  res.send("hello music");
+});
+
+app.use((req, res) => {
+  res.status(404).send("PAGE NOT FOUND!!");
 });
 
 app.listen(8080, onHTTPStart);
