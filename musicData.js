32e5128ef99221e8909b6ea8dacd608e9cc700d7
seog-jun <shong69@myseneca.ca>
2022-02-21 22:40:43 -0500

    Handlebar

--- musicData.js
+++ musicData.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 let albums = [];
 
-module.exports.getAlbums = function () {
+module.exports.initialize = function () {
   return new Promise((resolve, reject) => {
     fs.readFile("./data/albums.json", "utf-8", (error, data) => {
       if (error) {
@@ -10,8 +10,24 @@ module.exports.getAlbums = function () {
       } else {
         albums = JSON.parse(data);
         console.log(albums);
-        resolve(albums);
+        resolve();
       }
     });
+  });
+};
+module.exports.getAlbums = function () {
+  return new Promise((resolve, reject) => {
+    if (albums.length > 0) {
+      resolve(albums);
+    } else {
+      reject("no results returned");
+    }
+  });
+};
+module.exports.addAlbum = function (album) {
+  return new Promise((resolve, reject) => {
+    album.id = albums.length + 1;
+    albums.push(album);
+    resolve();
   });
 };
--- server.js
+++ server.js
@@ -1,23 +1,63 @@
 const express = require("express");
 const app = express();
+
+const env = require("dotenv");
+env.config(); // which indicates root directory (.env)
+
 const path = require("path");
 const musicData = require("./musicData.js");
 
-const HTTP_PORT = process.env.port || 8080;
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+const exphbs = require("express-handlebars");
+const { Z_STREAM_END } = require("zlib");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  secure: true,
+});
+
+const HTTP_PORT = process.env.PORT;
 const onHTTPStart = () => {
   console.log(`Express server is listening on port ${HTTP_PORT}🚀`);
 };
 
 app.use(express.static("public"));
 
+// for form data without file
+// app.use(express.urlencoded({ extended: true }));
+
+// multer middleware
+const upload = multer();
+
+// handle bars
+app.engine(".hbs", exphbs.engine({ extname: ".hbs", defaultLayout: "main" }));
+app.set("view engine", ".hbs");
+
 app.get("/", (req, res) => {
   res.redirect("/home");
 });
 
 app.get("/home", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  // res.sendFile(path.join(__dirname, "/views/index.html"));
+
+  musicData.getAlbums().then((data) => {
+    res.render("index", {
+      data: data,
+      layout: "main",
+    });
+  });
 });
 
+app.get("/music", (req, res) => {
+  musicData.getAlbums().then((data) => {
+    res.json(data);
+  });
+});
 app.get("/about", (req, res) => {
   res.send("hello about");
 });
@@ -62,8 +102,63 @@ app.get("/info/:id", (req, res) => {
     });
 });
 
+app.get("/albums", (req, res) => {
+  // res.sendFile(path.join(__dirname, "/views/albums.html"));
+  res.render("albums", {
+    data: null,
+    layout: "main",
+  });
+});
+
+app.post("/albums/new", upload.single("photo"), (req, res) => {
+  let streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+      let stream = cloudinary.uploader.upload_stream((error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      });
+
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+  };
+
+  async function upload(req) {
+    let result = await streamUpload(req);
+    console.log(result);
+    return result;
+  }
+
+  upload(req).then((uploaded) => {
+    req.body.imagePath = uploaded.url;
+    console.log(req.body);
+
+    musicData
+      .addAlbum(req.body)
+      .then((data) => {
+        res.redirect("/home");
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+
+    //res.send(JSON.stringify(req.body));
+    // req.body.featureImage = uploaded.url;
+  });
+});
+
 app.use((req, res) => {
   res.status(404).send("PAGE NOT FOUND!!");
 });
 
-app.listen(8080, onHTTPStart);
+//app.listen(8080, onHTTPStart);
+musicData
+  .initialize()
+  .then(() => {
+    app.listen(HTTP_PORT, onHTTPStart);
+  })
+  .catch((msg) => {
+    console.log(msg);
+  });
